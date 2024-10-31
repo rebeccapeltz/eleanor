87a61681836f22046ef444068284b7c08199b935
rebeccapeltz <rebeccapeltz@gmail.com>
2024-10-31 16:52:22 -0700

    add license;adjust colors

--- src/pages/Rules.jsx
+++ src/pages/Rules.jsx
@@ -15,30 +15,54 @@ function Rules() {
       <div className="intro">
         <h1>Digital Rogerian Therapy with Eleanor</h1>
         <p>
-          You are in Eleanor’s waiting room. You can enter a session with
-          Eleanor anytime by clicking the Session link in the navigation bar. 
-          Please read the notes and rules regarding a session
-          with Eleanor below.
+          Welcome to Eleanor’s waiting room. You can enter a session with
+          Eleanor anytime by clicking the <span className="bold">Session</span> link in the navigation bar.
+          When you converse with Eleanor, you will be referred to as the Client.
+        </p>
+        <p className="highlight">
+          Please read the notes regarding a session with Eleanor
+          below.
         </p>
 
-        <h2>Who is Eleanor?</h2>
+        <h2>Eleanor</h2>
 
         <p>
           Eleanor is a Rogerian Digital Therapist. She succeeds Eliza, who
-          pioneered Rogerian Digital Therapy in the 1960s.  
-          <a href="https://en.wikipedia.org/wiki/Joseph_Weizenbaum" 
-          target="_blank" rel="noreferrer"> Joseph Weizenbaum</a> developed 
-           <a href="https://en.wikipedia.org/wiki/ELIZA" 
-          target="_blank" rel="noreferrer"> Eliza</a> at the 
-          Massachusetts Institute of Technology to investigate communication 
-          between humans and machines. 
+          pioneered Rogerian Digital Therapy in the 1960s.
+          <a
+            href="https://en.wikipedia.org/wiki/Joseph_Weizenbaum"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Joseph Weizenbaum
+          </a>{" "}
+          developed
+          <a
+            href="https://en.wikipedia.org/wiki/ELIZA"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Eliza
+          </a>{" "}
+          at the Massachusetts Institute of Technology to investigate
+          communication between humans and machines.
         </p>
 
-        <p>Eleanor responds to client prompts using Goole's gemini-1.5-flash model.
-          Learn more about Google Gemini by reading the <a href="https://gemini.google.com/faq" target="_blank" rel="noreferrer">FAQ</a>
+        <p>
+          Eleanor responds to client prompts using Goole's gemini-1.5-flash
+          model. Learn more about Google Gemini by reading the{" "}
+          <a
+            href="https://gemini.google.com/faq"
+            target="_blank"
+            rel="noreferrer"
+          >
+            FAQ
+          </a>
         </p>
 
-        <h2>What is Rogerian Therapy?</h2>
+        <h2>Rogerian Therapy</h2>
         <p>
           Rogerian therapy focuses on helping the client solve personal problems
           through self-reflection. The therapist does not try to influence the
@@ -46,9 +70,7 @@ function Rules() {
           the client to explore life challenges, feelings, and goals.
         </p>
 
-        <h2>
-          What Happens to the Information Exchanged by Eleanor and the Client?
-        </h2>
+        <h2>Information Exchanged by Eleanor and the Client</h2>
 
         <p>
           This application does not save data locally or in the cloud. It
@@ -57,18 +79,29 @@ function Rules() {
           information gathered there is retained.
         </p>
 
-        <h2 className="bannedwords-title">Banned Words</h2>
+        <h2 className="highlight">Banned Words</h2>
         <p>
           The conversation with Eleanor is not intended to solve mental health
-          problems. If you need mental health therapy, look into 
-          <a href="https://www.nami.org/" target="_blank" rel="noreferrer"> NAMI</a>. 
-          If you are suicidal, look into 
-          <a href="https://www.nimh.nih.gov/health/topics/suicide-prevention" target="_blank" rel="noreferrer"> Suicide Prevention</a>.
-          To keep the conversation focused away from topics not relevant to Eleanor’s
-          Therapy, a set of words is banned for use in conversation. If the client uses 
-          any of these words, an Admin will let them know that they have used
-          banned words and must rephrase them. Prompts will banned words will not be 
-          forwarded to Eleanor. The list of banned words is shown below.
+          problems. If you need mental health therapy, look into
+          <a href="https://www.nami.org/" target="_blank" rel="noreferrer">
+            {" "}
+            NAMI
+          </a>
+          . If you are suicidal, look into
+          <a
+            href="https://www.nimh.nih.gov/health/topics/suicide-prevention"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Suicide Prevention
+          </a>
+          . To keep the conversation focused away from topics not relevant to
+          Eleanor’s Therapy, a set of words is banned for use in conversation.
+          If the client uses any of these words, an Admin will let them know
+          that they have used banned words and must rephrase them. Prompts will
+          banned words will not be forwarded to Eleanor. The list of banned
+          words is shown below.
         </p>
 
         <div>
@@ -76,7 +109,6 @@ function Rules() {
         </div>
         <Footer />
       </div>
-     
     </>
   );
 }
